3ca903e621e79f8e5462405c64ab599d8b7756fb
Alif Panglima Nurda <alifnurda@gmail.com>
2021-05-11 05:25:16 +0700

    First Commit

---
+++ js/class-calculator.js
@@ -0,0 +1,31 @@
+class Calculator {
+  constructor () {
+
+  }
+
+  add () {
+
+  }
+
+  sub () {
+
+  }
+
+  mul () {
+
+  }
+
+  div () {
+    
+  }
+
+  hasil () {
+
+  }
+
+  hapus () {
+    
+  }
+}
+
+export {Calculator};
---
+++ js/script.js
@@ -0,0 +1,8 @@
+ // Memanggil Class Calculator
+import {Calculator} from  './class-calculator';
+
+// Mengambil data Class numbers pada html
+const num = document.querySelectorAll('.numbers');
+
+// Mengambil data Class operators pada html
+const op = document.querySelectorAll('.operators');
